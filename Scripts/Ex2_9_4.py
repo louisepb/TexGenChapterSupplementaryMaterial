9ae07f0be19aaf12e78ebc3658b9e2a5e5342d89
Louise Brown <louise.brown@nottingham.ac.uk>
2022-02-25 13:49:55 +0000

    Added scripts and TexGen models

---
+++ Scripts/Ex2_9_4.py
@@ -0,0 +1,47 @@
+# Create a textile
+Textile = CTextile()
+
+# Create a python list containing 4 yarns
+Yarns = [CYarn(), CYarn(), CYarn(), CYarn()]
+
+# Add nodes to the yarns to describe their paths
+Yarns[0].AddNode(CNode(XYZ(0, 0, 0)))
+Yarns[0].AddNode(CNode(XYZ(0.22, 0, 0.05)))
+Yarns[0].AddNode(CNode(XYZ(0.44, 0, 0)))
+
+Yarns[1].AddNode(CNode(XYZ(0, 0.22, 0.05)))
+Yarns[1].AddNode(CNode(XYZ(0.22, 0.22, 0)))
+Yarns[1].AddNode(CNode(XYZ(0.44, 0.22, 0.05)))
+
+Yarns[2].AddNode(CNode(XYZ(0, 0, 0.05)))
+Yarns[2].AddNode(CNode(XYZ(0, 0.22, 0)))
+Yarns[2].AddNode(CNode(XYZ(0, 0.44, 0.05)))
+
+Yarns[3].AddNode(CNode(XYZ(0.22, 0, 0)))
+Yarns[3].AddNode(CNode(XYZ(0.22, 0.22, 0.05)))
+Yarns[3].AddNode(CNode(XYZ(0.22, 0.44, 0)))
+
+# Loop over all the yarns in the list
+for Yarn in Yarns:
+
+    # Set the interpolation function
+    Yarn.AssignInterpolation(CInterpolationCubic())
+
+    # Assign a constant cross-section along the yarn of lenticular shape
+    Yarn.AssignSection(CYarnSectionConstant(CSectionLenticular(0.2, 0.048)))
+
+    # Set the resolution of the mesh created
+    Yarn.SetResolution(40)
+
+    # Add repeat vectors to the yarn
+    Yarn.AddRepeat(XYZ(0.44, 0, 0))
+    Yarn.AddRepeat(XYZ(0, 0.44, 0))
+
+    # Add the yarn to our textile
+    Textile.AddYarn(Yarn)
+
+# Create a domain and assign it to the textile
+Textile.AssignDomain(CDomainPlanes(XYZ(0, 0, -0.024), XYZ(0.44, 0.44, 0.074)))
+
+# Add the textile with the name "polyester"
+AddTextile("polyester", Textile)
